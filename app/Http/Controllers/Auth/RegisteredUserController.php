f3f9892eed551bbe4af810c6164e827bd22dbe0c
acferten <gowiththeirname@gmail.com>
2024-01-09 12:47:54 +0700

    fix

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Helpers\ImageHelper;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $maxSize = 2048; //kb
+        $allowedTypes = 'jpeg,jpg,png,gif';
+
+        $request->validate([
+            'name' => ['nullable', 'string', 'max:255'],
+            'surname' => ['nullable', 'string', 'max:255'],
+            'nickname' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'photo' => ['nullable', 'mimes:' . $allowedTypes, 'max:' . $maxSize],
+            'about_yourself' => ['nullable', 'string'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name ?? null,
+            'surname' => $request->surname ?? null,
+            'nickname' => $request->nickname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'photo' => $request->hasFile('photo') ? $request->file('photo') : null,
+            'about_yourself' => $request->about_yourself ?? null,
+        ]);
+        ImageHelper::resaveImages($user);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,29 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ProfileController;
+use App\Models\Pages;
+use App\Modules\News\Controllers\NewsController;
+use App\Modules\User\Controllers\UserController;
+use App\Modules\Work\Controllers\WorkController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('main');
 })->name('main');
+
+Route::get('/about_the_contest', [Pages::class, 'about_the_contest'])->name('about_the_contest');
 
 Route::get('/how_to_become_a_member', function () {
     return view('how_to_become_a_member');
@@ -19,10 +37,24 @@ Route::get('/partners', function () {
     return view('partners');
 })->name('partners');
 
+Route::get('/news', [NewsController::class, 'list'])->name('news');
+Route::get('/news/{url}', [NewsController::class, 'card'])->name('news.card');
 
 Route::get('/feedback', function () {
     return view('feedback');
 })->name('feedback');
+
+
+Route::get('/gallery', [WorkController::class, 'list'])->name('gallery');
+Route::get('/gallery/{slug}', [WorkController::class, 'card'])->name('gallery.card');
+Route::get('/vote/{id}', [WorkController::class, 'vote'])->name('vote');
+//Route::get('/gallery', function () {
+//    return view('gallery');
+//})->name('gallery');
+
+Route::get('/participants', [UserController::class, 'list'])->name('participants');
+
+Route::get('/participants_rating', [WorkController::class, 'rating'])->name('participants_rating');
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -32,4 +64,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-//require __DIR__ . '/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'card'])->name('profile.card');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/edit', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/edit', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('add-work', [WorkController::class, 'create'])
+        ->name('work.create');
+    Route::post('add-work', [WorkController::class, 'store'])
+        ->name('work.store');
+});
+
+require __DIR__ . '/auth.php';
+
+Route::get('/delete/image',  [Controller::class, 'deleteImage']);
+Route::post('/crop/image', [Controller::class, 'cropImage']);
+Route::any('/render/image',  [Controller::class, 'renderFile']);
