260fa1d4c6d6e3360899a4328c1e234e3e0e6b36
acferten <gowiththeirname@gmail.com>
2024-01-24 23:19:28 +0700

    work card

--- resources/views/works/work.blade.php
+++ resources/views/works/work.blade.php
@@ -7,22 +7,21 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <h1>Картина «{{ $work->title }}»</h1>
-
-                        <p>{{$work->genre->name }}, {{$work->year }} год</p>
+                        <h1 class="work-title">
+                            {{$work->title}}
+                        </h1>
+                        <p class="subtitle my-4">{{$work->genre->name }}, {{$work->year }} год</p>
                     </div>
                     <div class="container work">
-                        <div class="row">
-                            <div class="col-3 work_photo">
-                                <div class="img">
-                                    <img
-                                        src="{{ $work->getWorkFileUrl() }}"
-                                        alt="{{ $work->title }}">
+                        <div class="d-flex row flex-wrap justify-content-between">
+                            <div class="d-flex flex-column col-lg-3 col-md-4 col-sm-12 col-12 work_photo">
+                                <div class="w-100 h-100">
+                                    <img class="w-100" src="{{ $work->getWorkFileUrl() }}" alt="{{ $work->title }}">
                                 </div>
-                                <div class="row">
+                                <div class="row py-4">
                                     <div class="col">
                                         <div
-                                            class="work__rating js-work__rating js-work_{{ $work->id }}_rating">{{$work->votes()->count()}}</div>
+                                            class="work__rating">{{$work->votes()->count()}}</div>
                                         <div
                                             class="work__rating-unit">{{ trans_choice('validation.votes', $work->votes()->count()) }}</div>
                                     </div>
@@ -34,13 +33,15 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="offset-1 col-8 work_data">
-                                <div class="name">
+
+                            <div
+                                class="offset-lg-1 col-lg-8 col-md-8 col-sm-12 col-12 work_data d-flex flex-column justify-content-between">
+                                <div class="name my-4">
                                     {{ $work->user->first_name }} {{ $work->user->last_name }}
                                 </div>
 
                                 @if($other_works)
-                                    <div class="row">
+                                    <div class="row py-4">
                                         <div class="col-12">
                                             Работы автора:
                                             <ul class="work__other-work">
