54c43a5216ee4da43c0b771c941560648fe5ec4f
Chernogrivov Kirill <kirill.2201@inbox.ru>
2024-01-24 21:27:30 +0700

    footer styles

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="mt-auto navbar-fixed-bottom d-flex align-items-center">
-    <div class="container-fluid">
-        <div class="row text-center flex-fill">
-            <div class="col col-md-4 col-12 d-flex align-items-center justify-content-center"><a href="{{ route('gallery') }}">Галерея</a></div>
-            <div class="col col-md-4 col-12 d-flex align-items-center justify-content-center"><a href="{{ route('participants.index') }}">Участники</a></div>
-            <div class="col col-md-4 col-12 d-flex align-items-center justify-content-center"><a href="{{ route('participants_rating') }}">Рейтинг участников</a></div>
+    <div class="container">
+        <div class="row text-center justify-content-between">
+            <div class="col col-md-3 col-12 d-flex align-items-center justify-content-start"><a href="{{ route('gallery') }}">Галерея</a></div>
+            <div class="col col-md-6 col-12 d-flex align-items-center justify-content-center"><a href="{{ route('participants_rating') }}">Рейтинг участников</a></div>
+            <div class="col col-md-3 col-12 d-flex align-items-center justify-content-end"><a href="{{ route('participants.index') }}">Участники</a></div>
         </div>
     </div>
 </footer>
