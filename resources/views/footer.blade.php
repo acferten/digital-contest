0e86355c652e3816573a013b123f4ae3163b77b0
Chernogrivov Kirill <kirill.2201@inbox.ru>
2024-02-05 03:56:17 +0700

    fix footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,14 @@
-<footer class="mt-auto navbar-fixed-bottom d-flex align-items-center">
+<footer class="mt-auto navbar-fixed-bottom d-flex align-items-center footer">
     <div class="container">
-        <div class="row text-center justify-content-between">
-            <div class="col col-md-3 col-12 d-flex align-items-center justify-content-start"><a href="{{ route('gallery') }}">Галерея</a></div>
-            <div class="col col-md-6 col-12 d-flex align-items-center justify-content-center"><a href="{{ route('participants_rating') }}">Рейтинг участников</a></div>
-            <div class="col col-md-3 col-12 d-flex align-items-center justify-content-end"><a href="{{ route('participants.index') }}">Участники</a></div>
+        <div class="row">
+            <div class="text-center d-flex justify-content-between flex-wrap footer__links">
+                <div class="col col-md-3 col-12 d-flex align-items-center justify-content-start footer__link" ><a
+                        href="{{ route('gallery') }}">Галерея</a></div>
+                <div class="col col-md-6 col-12 d-flex align-items-center justify-content-center footer__link"><a
+                        href="{{ route('participants_rating') }}">Рейтинг участников</a></div>
+                <div class="col col-md-3 col-12 d-flex align-items-center justify-content-end footer__link"><a
+                        href="{{ route('participants.index') }}">Участники</a></div>
+            </div>
         </div>
     </div>
 </footer>
