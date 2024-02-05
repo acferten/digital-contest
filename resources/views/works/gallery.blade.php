811ecede8d412b0be871d4928342148b649dc7b3
Kirill Chernogrivov <37214000+KChernogrivov@users.noreply.github.com>
2024-02-05 18:39:23 +0700

    delete gray from gallery.blade.php and participants_rating.blade.php (#4)

--- resources/views/works/gallery.blade.php
+++ resources/views/works/gallery.blade.php
@@ -1,11 +1,8 @@
 @extends('layout')
 @section('title', 'Галерея')
-@section('body_type', 'background_type4_1')
+@section('body_type', 'background_type6')
+{{--@section('body_type', 'background_type4_1')--}}
 @section('background')
-    <video autoplay muted loop id="background">
-        <source src="dist/video/ArtNFT_Fon1_Gorizont.webm" type="video/mp4">
-        Your browser does not support HTML5 video.
-    </video>
 @endsection
 @section('content')
     <main class="flex-grow-1" style="margin-top: -2px">
@@ -14,31 +11,36 @@
                 <div class="col-8">
                     <h1>Галерея</h1>
                 </div>
-                <div class="col-4">
-                    <a href="#" class="btn btn-danger float-end mt-3">Поиск</a>
-                </div>
             </div>
             <div class="gallery">
                 <div class="row">
-
                     @foreach($works as $work)
                         <div class="col-lg-3 col-md-4 col-sm-6 col-12 item">
-                            <div class="img">
-                                <img
-                                    src="{{ $work->getWorkFileUrl() }}"
-                                    alt="{{ $work->title }}">
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                    <div class="title"><a
-                                            href="{{ route('works.show', [$work->id]) }}"
-                                        >{{ $work->title }}</a>
-                                    </div>
-                                    <div class="description">
-                                        <span class="js-work__rating js-work_{{ $work->id }}_rating">{{ $work->votes()->count() }}</span> {{ trans_choice('validation.votes', $work->votes()->count()) }}</div>
+                            <a href="{{ route('works.show', [$work->id]) }}">
+                                <div class="img">
+                                    <img
+                                        src="{{ $work->getWorkFileUrl() }}"
+                                        alt="{{ $work->title }}">
                                 </div>
-                                <x-vote-button :$work/>
-                            </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="title">{{ $work->title }}</div>
+                                    </div>
+                                </div>
+                                <div class="row mt-2">
+                                    <div class="col">
+                                        <div class="description d-flex flex-column items-start">
+                                            <span
+                                                class="js-work__rating js-work_{{ $work->id }}_rating">{{ $work->votes()->count() }}
+                                            </span>
+                                            {{ trans_choice('validation.votes', $work->votes()->count()) }}
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <x-vote-button :$work/>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
                     @endforeach
                 </div>
