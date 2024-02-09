2e87aa868cb50effc488e8988907ef5d356e5ee1
Kirill Chernogrivov <37214000+KChernogrivov@users.noreply.github.com>
2024-02-10 01:10:48 +0700

    Profile responsive (#7)
    * profile responsive
    
    * fix
    
    * fix

--- resources/views/profile/card.blade.php
+++ resources/views/profile/card.blade.php
@@ -6,7 +6,7 @@
         <div class="main-text">
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 profile-title">
                         <h1>Профиль</h1>
                     </div>
                     <div class="container profile profile--desktop">
@@ -24,7 +24,7 @@
                             </div>
 
                             <div
-                                    class="offset-lg-1 col-lg-8 col-md-8 col-sm-12 col-12 profile_data d-flex flex-column">
+                                class="offset-lg-1 col-lg-8 col-md-8 col-sm-12 col-12 profile_data d-flex flex-column">
                                 <div class="name mb-4">
                                     {{ $user->first_name }}<br>
                                     {{ $user->last_name }}
@@ -36,7 +36,7 @@
                                             приз</a>
                                     </div>
                                     <div class="col">
-                                        <a href="{{ route('products.edit', 1) }}" class="btn btn-danger">Изменить цену
+                                        <a href="{{ route('products.edit', 2) }}" class="btn btn-danger">Изменить цену
                                             голосования</a>
                                     </div>
                                 </div>
@@ -47,7 +47,7 @@
                                     </div>
 
                                     <div class="col">
-                                        <a href="{{ route('products.edit', 2) }}" class="btn btn-danger">Изменить
+                                        <a href="{{ route('products.edit', 1) }}" class="btn btn-danger">Изменить
                                             цену
                                             размещения</a>
                                     </div>
@@ -60,7 +60,7 @@
                                 </div>
                                 @else
                                     <div
-                                            class="col-lg-9 col-md-4 col-sm-12 col-12 d-flex flex-column w-100 justify-content-between flex-wrap flex-grow-1 justify-content-between">
+                                        class="col-lg-9 col-md-4 col-sm-12 col-12 d-flex flex-column w-100 justify-content-between flex-wrap flex-grow-1 justify-content-between">
 
                                         <p class="w-100 mb-4" style="font-size: 0.9rem;">{{ $user->about }}</p>
 
@@ -77,15 +77,31 @@
                         </div>
                     </div>
                     <div class="container profile profile--mobile">
-                        <div class="row">
+                        <div class="w-100">
                             <div class="d-flex justify-content-center profile_photo">
                                 <img src="{{ $user->getProfilePictureUrl() }}" alt="profile picture">
                             </div>
                             <div
-                                    class="offset-lg-1 col-lg-8 col-md-8 col-sm-12 col-12 profile_data d-flex flex-column">
+                                class="w-100 profile_data d-flex flex-column">
                                 <div class="name text-center mt-4 mb-4">
                                     {{ $user->first_name }}<br>
                                     {{ $user->last_name }}
+                                </div>
+                                <div
+                                    class="col-lg-9 col-md-4 col-sm-12 col-12 d-flex flex-column w-100 justify-content-between flex-wrap flex-grow-1 justify-content-between">
+                                    <p class="w-100 mb-4" style="font-size: 0.9rem;">{{ $user->about }}</p>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col w-100">
+                                        <a href="{{ route('avatar.edit') }}" class="w-100 btn btn-danger">Обновить
+                                            фотографию</a>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col w-100">
+                                        <a href="{{ route('profile.edit') }}" class="w-100 btn btn-danger">Редактировать
+                                            профиль</a>
+                                    </div>
                                 </div>
                                 @role('admin')
                                 <div class="row mb-3">
@@ -96,7 +112,7 @@
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col">
-                                        <a href="{{ route('products.edit', 1) }}" class="btn btn-danger w-100">Изменить
+                                        <a href="{{ route('products.edit', 2) }}" class="btn btn-danger w-100">Изменить
                                             цену
                                             голосования</a>
                                     </div>
@@ -109,7 +125,7 @@
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col">
-                                        <a href="{{ route('products.edit', 2) }}" class="btn btn-danger w-100">Изменить
+                                        <a href="{{ route('products.edit', 1) }}" class="btn btn-danger w-100">Изменить
                                             цену
                                             размещения</a>
                                     </div>
@@ -121,13 +137,22 @@
                                     </div>
                                 </div>
                                 @else
-                                    <div
-                                            class="col-lg-9 col-md-4 col-sm-12 col-12 d-flex flex-column w-100 justify-content-between flex-wrap flex-grow-1 justify-content-between">
-                                        <p class="w-100 mb-4" style="font-size: 0.9rem;">{{ $user->about }}</p>
-                                        <div class="subtitle d-flex flex-column justify-content-between">
-                                            <a href="{{ route('profile.works') }}" class="btn btn-danger w-100 mb-4">Ваши
+                                    <div class="row mb-3">
+                                        <div class="col w-100">
+                                            <a href="{{ route('works.create') }}" class="w-100 btn btn-danger w-100">Добавить
+                                                работу</a>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col w-100">
+                                            <a href="{{ route('profile.works') }}" class="btn btn-danger w-100">Ваши
                                                 работы</a>
-                                            <a href="{{ route('profile.payments') }}" class="btn btn-danger w-100">Ваши оплаты</a>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col w-100">
+                                            <a href="{{ route('profile.payments') }}" class="btn btn-danger w-100">Ваши
+                                                оплаты</a>
                                         </div>
                                     </div>
                                     @endrole
