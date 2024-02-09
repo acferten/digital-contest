ccd1a291014447b4bc1b477f79eba8342938446b
Chernogrivov Kirill <kirill.2201@inbox.ru>
2024-02-10 01:02:56 +0700

    fix

--- resources/views/profile/card.blade.php
+++ resources/views/profile/card.blade.php
@@ -6,7 +6,7 @@
         <div class="main-text">
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 profile-title">
                         <h1>Профиль</h1>
                     </div>
                     <div class="container profile profile--desktop">
@@ -77,15 +77,27 @@
                         </div>
                     </div>
                     <div class="container profile profile--mobile">
-                        <div class="row">
+                        <div class="w-100">
                             <div class="d-flex justify-content-center profile_photo">
                                 <img src="{{ $user->getProfilePictureUrl() }}" alt="profile picture">
                             </div>
                             <div
-                                class="offset-lg-1 col-lg-8 col-md-8 col-sm-12 col-12 profile_data d-flex flex-column">
+                                class="w-100 profile_data d-flex flex-column">
                                 <div class="name text-center mt-4 mb-4">
                                     {{ $user->first_name }}<br>
                                     {{ $user->last_name }}
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col w-100">
+                                        <a href="{{ route('avatar.edit') }}" class="w-100 btn btn-danger">Обновить
+                                            фотографию</a>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="col w-100">
+                                        <a href="{{ route('profile.edit') }}" class="w-100 btn btn-danger">Редактировать
+                                            профиль</a>
+                                    </div>
                                 </div>
                                 @role('admin')
                                 <div class="row mb-3">
@@ -120,11 +132,19 @@
                                     </div>
                                 </div>
                                 @else
-                                    <div
-                                        class="col-lg-9 col-md-4 col-sm-12 col-12 d-flex flex-column w-100 justify-content-between flex-wrap flex-grow-1 justify-content-between">
-                                        <p class="w-100 mb-4" style="font-size: 0.9rem;">{{ $user->about }}</p>
-                                        <div class="subtitle d-flex flex-column justify-content-between">
-                                            <a href="{{ route('profile.works') }}" class="btn btn-danger w-100 mb-4">Ваши работы</a>
+                                    <div class="row mb-3">
+                                        <div class="col w-100">
+                                            <a href="{{ route('works.create') }}" class="w-100 btn btn-danger w-100">Добавить
+                                                работу</a>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col w-100">
+                                            <a href="{{ route('profile.works') }}" class="btn btn-danger w-100">Ваши работы</a>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3">
+                                        <div class="col w-100">
                                             <a href="{{ route('profile.payments') }}" class="btn btn-danger w-100">Ваши оплаты</a>
                                         </div>
                                     </div>
