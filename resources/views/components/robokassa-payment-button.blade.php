0ef13e1c8b45635f9bf3128fc71b98ee0297b2a6
acferten <gowiththeirname@gmail.com>
2024-05-26 00:02:53 +0700

    fix unique inv id

--- resources/views/components/robokassa-payment-button.blade.php
+++ resources/views/components/robokassa-payment-button.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\DB; @endphp
 @props(['work', 'price' => null])
 
 @auth('sanctum')
@@ -7,24 +8,21 @@
         ->where('payments.status', \Domain\Products\Enums\OrderStatus::Paid)
         ->where('payments.work_id', $work->id)
         ->whereDate('payments.created_at', '=', date('Y-m-d'))
-        ->sum('payments.price') >= 500
-        ){
+        ->sum('payments.price') >= 500) {
             \Illuminate\Support\Facades\Redirect::route('works.show', $work)->with('success', 'Вы не можеет');
         }
-
-
-
-
             $product = Domain\Products\Models\Product::where('name', \Domain\Products\Enums\ProductEnum::Voting->value)->first();
             $merchant_login = env('ROBOKASSA_LOGIN');
             $password_1 = env('ROBOKASSA_PASSWORD_1');
             $description = \Domain\Products\Enums\ProductEnum::Voting->value;
             $out_sum = $price ?? $product->price;
             $user_id = auth()->user()->id;
-            $inv_id = (int)(((int)($work->id . $user_id . time())) / 1000000);
+            $inv_id = (int)$work->id . $user_id . (time() % 1000);
+            while (DB::table('payments')->where('invoice_id', $inv_id)->exists()) {
+                $inv_id = (int)$work->id . $user_id . fake()->randomNumber(3);
+            }
             $signature_value = md5("{$merchant_login}:{$out_sum}:{$inv_id}:{$password_1}:Shp_ProductId={$product->id}:Shp_UserId={$user_id}:Shp_WorkId={$work->id}");
     @endphp
-
 
     <div class="col">
         <form action="https://auth.robokassa.ru/Merchant/Index.aspx" method="get">
